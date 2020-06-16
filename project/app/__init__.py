dab1e0d2b7e585ccc4b0ab7f781d96f4d856cf6d
Mustafa Ahmed <mmustafaadn@gmail.com>
2020-06-16 06:01:46 +0200

    better config

--- project/app/__init__.py
+++ project/app/__init__.py
@@ -5,6 +5,9 @@ def create_app():
 	from . import models #, routes, services
 	app = Flask(__name__)
 
+	# Configurations
+	app.config.from_object('config')
+
 	models.init_app(app)
 	#routes.init_app(app)
 	#services.init_app(app)
--- project/run.py
+++ project/run.py
@@ -5,11 +5,8 @@ from dotenv import load_dotenv
 
 if __name__ == "__main__":
 
-	# load the variables from the env file !
+	# load the variables from the env file!
 	load_dotenv()
 
-	# Configurations
-	fullApp.config.from_object('config')
-	
-	# Run a test server.
+	# Run the server.
 	fullApp.run(host='0.0.0.0', port=8080)
