86a80863ab58629f7d62d7f849fab11416645f68
Mustafa Ahmed <mmustafaadn@gmail.com>
2020-06-22 21:24:18 +0200

    add 'werkzeug' module

--- project/app/extensions.py
+++ project/app/extensions.py
@@ -2,4 +2,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 from dotenv import load_dotenv
 
+from werkzeug.security import check_password_hash, generate_password_hash
+from werkzeug.utils import secure_filename
+
+
 import datetime
