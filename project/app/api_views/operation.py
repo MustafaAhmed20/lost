d5c6ca27add2ddeee14d19002abe09081422e098
Mustafa Ahmed <mmustafaadn@gmail.com>
2020-07-05 19:35:55 +0200

    add 'addoperation' route, add 'getoperation' route , modify 'addcountry' route - add and modify testing

--- project/app/api_views/operation.py
+++ project/app/api_views/operation.py
@@ -1,27 +1,24 @@
 from . import api, current_app, jsonify, request, make_response, json
-from . import status, baseApi
-from . import adminRequired, loginRequired
+from . import status, baseApi, jwt
+from . import adminRequired, loginRequired, saveFile, MAX_IMEGES_NUMBER
 
 # the logic
-from . import addOperation, addCountry, getCountry, getStatus_operation, getType_operation
-
-@api.route('/addoperation', methods=['POST'])
-@loginRequired
-def addOperationRoute():
-	pass
-
+from . import addOperation, addCountry, getCountry, getStatus_operation, getType_operation, getOperation
+from . import addPerson, addPhoto, deletePerson
+from . import getUser
+import datetime
 
 @api.route('/addcountry', methods=['POST'])
-@loginRequired
+@adminRequired
 def addCountryRoute():
-	# the retuned response
+	# the returned response
 	result = baseApi.copy()
 
 	# get the post data
 	post_data = request.get_json()
 
 	countryName = post_data.get('name')
-	countryPhoneCode = post_data.get('phonecode')
+	countryPhoneCode = post_data.get('phone_code')
 	
 	
 	if not countryName or not countryPhoneCode:
@@ -32,7 +29,7 @@ def addCountryRoute():
 	# make sure the user not already exists
 	if getCountry(phoneCode=countryPhoneCode):
 		result['status'] = status['failure']
-		result['message'] = 'Country already exists. Please Log in.'
+		result['message'] = 'Country already exists.'
 		return make_response(jsonify(result), 202)
 
 	if not addCountry(name=countryName, phoneCode=countryPhoneCode):
@@ -106,7 +103,7 @@ def getStatusOperationRoute(status_id=None):
 
 		# success
 		result['status'] = status['success']
-		result['data']['statusoperation'] = [s.toDict()]
+		result['data']['status_operation'] = [s.toDict()]
 		return make_response(jsonify(result), 200)
 
 	else:
@@ -116,7 +113,7 @@ def getStatusOperationRoute(status_id=None):
 
 
 		result['status'] = status['success']
-		result['data']['statusoperation'] = [c.toDict() for c in s]
+		result['data']['status_operation'] = [c.toDict() for c in s]
 		return make_response(jsonify(result), 200)
 
 
@@ -144,7 +141,7 @@ def getTypeOperationRoute(type_id=None):
 
 		# success
 		result['status'] = status['success']
-		result['data']['typeoperation'] = [type.toDict()]
+		result['data']['type_operation'] = [type.toDict()]
 		return make_response(jsonify(result), 200)
 
 	else:
@@ -154,5 +151,222 @@ def getTypeOperationRoute(type_id=None):
 
 
 		result['status'] = status['success']
-		result['data']['typeoperation'] = [c.toDict() for c in type]
+		result['data']['type_operation'] = [c.toDict() for c in type]
 		return make_response(jsonify(result), 200)
+
+
+# helper functions to add objects
+def addObjectPerson(post_data):
+	""" get the prson data. return False if add person failed. 
+		return the person object if added"""
+	personName = post_data.get('person_name')
+	age_id = post_data.get('age_id')
+
+	if not personName:
+		return False
+
+	# add person
+	person = addPerson(name=personName, ageId=age_id)
+	if not person:
+		return False
+
+	return person
+
+def deleteObjectPerson(object):
+	''' delete object'''
+
+	return deletePerson(object=object)
+
+
+object_types_add = {'Person':addObjectPerson}
+object_types_delete = {'Person':deleteObjectPerson}
+
+@api.route('/addoperation', methods=['POST'])
+@loginRequired
+def addOperationRoute():
+	""" add new operation"""
+
+	# the returned response
+	result = baseApi.copy()
+
+	# get the post data
+	post_data = request.form
+
+	# get the photos
+	files = request.files.getlist('photos')
+	
+	# limit of the uploaded imeges
+	if len(files) > MAX_IMEGES_NUMBER:
+		result['status'] = status['failure']
+		result['message']  = f'Large number of photos. no more than {MAX_IMEGES_NUMBER} photos'
+		return make_response(jsonify(result), 400)
+
+
+	# the user
+	token = request.headers.get('token')
+	try:
+		payload = jwt.decode(token, current_app.config.get('SECRET_KEY'), algorithms=['HS256'])
+		userPublicId = payload['user']
+	except jwt.ExpiredSignatureError :
+		result['status'] = status['failure']
+		result['message']  = 'Signature expired. Please log in again.'
+		return make_response(jsonify(result), 202)
+	except jwt.InvalidTokenError:
+		result['status'] = status['failure']
+		result['message']  ='Invalid token. Please log in again.'
+		return make_response(jsonify(result), 202)
+	
+
+	# get 'operation-dependent' data and get sure it's valid
+	date = post_data.get('date')
+	if not date:
+		result['status'] = status['failure']
+		result['message']  = "required data 'date' not submitted"
+		return make_response(jsonify(result), 400)
+
+	# validate the date
+	try:
+		dateObject = datetime.datetime.strptime(date, '%Y-%m-%d')
+	except Exception as e:
+		result['status'] = status['failure']
+		result['message']  = 'wrong date format. date must be in %Y-%m-%d fomat'
+		return make_response(jsonify(result), 400)
+
+	type_id = post_data.get('type_id')
+	type = getType_operation(id=type_id)
+	if not type or not type_id:
+		result['status'] = status['failure']
+		result['message']  = 'wrong type operation name'
+		return make_response(jsonify(result), 400)
+	
+	status_id = post_data.get('status_id')
+	
+	if status_id:
+		s = getStatus_operation(id=status_id)
+	else:
+		s = None
+	
+	# country of the operation
+	country_id = post_data.get('country_id')
+	country = getCountry(id=country_id)
+	if not country:
+		result['status'] = status['failure']
+		result['message']  = 'wrong country name'
+		return make_response(jsonify(result), 400)
+
+
+	# first get the object data
+	object_type = post_data.get('object_type')
+	
+	if not object_type or object_type not in object_types_add:
+		result['status'] = status['failure']
+		result['message'] = 'required data not submitted'
+		return make_response(jsonify(result), 400)
+
+	# add the object data with helper function
+	object = object_types_add[object_type](post_data)
+
+	if not object :
+		result['status'] = status['failure']
+		result['message'] = 'Some error occurred. when add the object data'
+		return make_response(jsonify(result), 400)
+	
+	# location of the item 
+	lat = post_data.get('lat')
+	lng = post_data.get('lng')
+
+	# validate
+	try:
+		if lat or lng:
+			lat = float(lat)
+			lng = float(lng)
+	except Exception as e:
+		result['status'] = status['failure']
+		result['message'] = 'lat and lng must be float numbers'
+		return make_response(jsonify(result), 400)
+
+	operation = addOperation(country=country, object=object, userPublicId=userPublicId,\
+	 			date=dateObject, type=type, status=s, lat=lat, lng=lng)
+
+
+	if not operation:
+		# delete added object
+		object_types_delete[object_type](object)
+		
+		result['status'] = status['failure']
+		result['message'] = 'Some error occurred. Please try again'
+		return make_response(jsonify(result), 401)
+
+	# get the photos
+	if files:
+		for index, file in enumerate(files):
+			# get sure no to many imeges
+			if index + 1 > MAX_IMEGES_NUMBER:
+				break
+			
+			link = saveFile(file)
+			if link:
+				addPhoto(link=link, object=object)
+			
+
+
+	# success
+	result['status'] = status['success']
+	return make_response(jsonify(result), 201)
+
+
+@api.route('/getoperation', methods=['GET'])
+def getOperationRoute():
+	""" get a list of operations"""
+	result = baseApi.copy()
+	#filters = request.view_args
+	filters = request.args
+	if not filters:
+		# no filters
+		try:
+			operations = getOperation()	
+		except Exception as e:
+			result['status'] = status['failure']
+			result['message'] = 'Some error occurred. Please try again'
+			return make_response(jsonify(result), 401)
+
+		# success
+		result['status'] = status['success']
+		result['data']['operations'] = [operation.toDict() for operation in operations]
+		return make_response(jsonify(result), 200)
+
+	else:
+		# with filters
+		if 'user' in filters:
+			user = getUser(userPublicId=filters['user'])
+			
+			if not user:
+				result['status'] = status['failure']
+				result['message'] = "not valid filter 'user'"
+				return make_response(jsonify(result), 400)
+
+			filters['user_id'] = user.id
+			del filters['user']
+
+		if 'date' in filters:
+			# make sure the date have the rigth fromat
+			try:
+				filters['date'] = datetime.datetime.strptime(date, '%Y-%m-%d')
+			except Exception as e:
+				result['status'] = status['failure']
+				result['message']  = 'wrong date format. date must be in %Y-%m-%d fomat'
+				return make_response(jsonify(result), 400)
+
+		try:
+			operations = getOperation(**filters)
+		except Exception as e:
+			result['status'] = status['failure']
+			result['message']  = e
+			return make_response(jsonify(result), 400)
+		
+		# success
+		result['status'] = status['success']
+		result['data']['operations'] = [operation.toDict() for operation in operations]
+		return make_response(jsonify(result), 200)
+
+
--- project/tests/test_api_operation.py
+++ project/tests/test_api_operation.py
@@ -1,10 +1,124 @@
 from . import TestConfig
 from app.api_views.operation import *
+from app.logic.operation import getCountry
+from app.logic.person import getPerson, getPohto
+
 import json
 import os
+from io import BytesIO
 
 class TestOperationApi(TestConfig):
 
+	def test_addcountry(self):
+		# first login as admin
+		
+		admin_phone = os.getenv('admin_phone')
+		admin_password = os.getenv('admin_pass')
+		
+		if not admin_phone or not admin_password:
+			raise ValueError('Environment variables not found!')
+		
+		data = {'phone':admin_phone, 'password':admin_password}
+		
+		# post requset
+		result = self.client_app.post("/api/login", data=json.dumps(data), content_type='application/json')
+
+		data = json.loads(result.data.decode())
+
+		self.assertEqual(data['status'], 'success')
+		self.assertEqual(result.status_code, 200)
+
+		adminToken = data['data']['token']
+
+		# add new country
+		headers = {'token':adminToken}
+		data = {'name': 'usa', 'phone_code':1}
+
+		result = self.client_app.post("/api/addcountry", data=json.dumps(data),\
+									 headers=headers,content_type='application/json')
+
+		
+		data = json.loads(result.data.decode())
+
+		self.assertEqual(data['status'], 'success')		
+		self.assertEqual(data['message'],  None)
+		self.assertEqual(result.content_type,  'application/json')
+		self.assertEqual(result.status_code, 201)
+
+		
+		country = getCountry(name='usa')
+		self.assertTrue(country, 'no country')
+		self.assertEqual(country.name, 'usa', 'no country')
+		self.assertEqual(country.phone_code, 1, 'no country')
+
+	def test_addcountry2(self):
+		""" try add country without valid data"""
+		# first login as admin
+		
+		admin_phone = os.getenv('admin_phone')
+		admin_password = os.getenv('admin_pass')
+		
+		if not admin_phone or not admin_password:
+			raise ValueError('Environment variables not found!')
+		
+		data = {'phone':admin_phone, 'password':admin_password}
+		
+		# post requset
+		result = self.client_app.post("/api/login", data=json.dumps(data), content_type='application/json')
+
+		data = json.loads(result.data.decode())
+
+		self.assertEqual(data['status'], 'success')
+		self.assertEqual(result.status_code, 200)
+
+		adminToken = data['data']['token']
+
+		# add new country
+		headers = {'token':adminToken}
+		data = {}
+
+		result = self.client_app.post("/api/addcountry", data=json.dumps(data),\
+									 headers=headers,content_type='application/json')
+
+		
+		data = json.loads(result.data.decode())
+
+		self.assertEqual(data['status'], 'failure')		
+		self.assertEqual(data['message'],  'required data not submitted')
+		self.assertEqual(result.content_type,  'application/json')
+		self.assertEqual(result.status_code, 400)
+
+		
+		data = {'name': 'usa'}
+
+		result = self.client_app.post("/api/addcountry", data=json.dumps(data),\
+									 headers=headers,content_type='application/json')
+
+		
+		data = json.loads(result.data.decode())
+
+		self.assertEqual(data['status'], 'failure')		
+		self.assertEqual(data['message'],  'required data not submitted')
+		self.assertEqual(result.content_type,  'application/json')
+		self.assertEqual(result.status_code, 400)	
+
+
+		# already exists
+		
+		data = {'name': 'sudan', 'phone_code':249}
+
+		result = self.client_app.post("/api/addcountry", data=json.dumps(data),\
+									 headers=headers,content_type='application/json')
+
+
+		data = json.loads(result.data.decode())
+
+		self.assertEqual(data['status'], 'failure')		
+		self.assertEqual(data['message'],  'Country already exists.')
+		self.assertEqual(result.content_type,  'application/json')
+		self.assertEqual(result.status_code, 202)
+
+		
 	def test_getcountry(self):
 		""" test the /getcountry route"""
 
@@ -38,7 +152,7 @@ class TestOperationApi(TestConfig):
 		data = json.loads(result.data.decode())
 
 		self.assertEqual(data['status'], 'success')
-		self.assertTrue(data['data']['statusoperation'])
+		self.assertTrue(data['data']['status_operation'])
 		self.assertEqual(result.content_type,  'application/json')
 		self.assertEqual(result.status_code, 200)
 
@@ -50,7 +164,7 @@ class TestOperationApi(TestConfig):
 		data = json.loads(result.data.decode())
 
 		self.assertEqual(data['status'], 'success')
-		self.assertTrue(data['data']['statusoperation'])
+		self.assertTrue(data['data']['status_operation'])
 		self.assertEqual(result.content_type,  'application/json')
 		self.assertEqual(result.status_code, 200)
 
@@ -63,7 +177,7 @@ class TestOperationApi(TestConfig):
 		data = json.loads(result.data.decode())
 
 		self.assertEqual(data['status'], 'success')
-		self.assertTrue(data['data']['typeoperation'])
+		self.assertTrue(data['data']['type_operation'])
 		self.assertEqual(result.content_type,  'application/json')
 		self.assertEqual(result.status_code, 200)
 
@@ -75,6 +189,162 @@ class TestOperationApi(TestConfig):
 		data = json.loads(result.data.decode())
 
 		self.assertEqual(data['status'], 'success')
-		self.assertTrue(data['data']['typeoperation'])
+		self.assertTrue(data['data']['type_operation'])
 		self.assertEqual(result.content_type,  'application/json')
 		self.assertEqual(result.status_code, 200)
+
+
+class TestOperationApi2(TestConfig):
+
+	def test_addoperation(self):
+		""" add new operation"""
+
+		# first log-in
+		admin_phone = os.getenv('admin_phone')
+		admin_password = os.getenv('admin_pass')
+
+		data = {'phone':admin_phone, 'password':admin_password}
+	
+		result = self.client_app.post("/api/login", data=json.dumps(data), content_type='application/json')
+
+		data = json.loads(result.data.decode())
+		
+		token = data['data']['token']
+
+		self.assertEqual(data['status'], 'success')
+		self.assertEqual(result.status_code, 200)
+
+		# add new operation
+		headers = {'token':token}
+		file_path = os.path.join(current_app.config['UPLOAD_FOLDER'], os.getenv('IMEGE_TEST_NAME'))
+		
+		
+		# send 3 photos
+		photos = []
+		for _ in range(3):
+			photos.append((open(file_path, 'rb'), os.getenv('IMEGE_TEST_NAME')))
+		
+		
+		
+		data = {'photos':photos, 'date':'2020-11-15',
+				'type_id':2, 'country_id':1,
+				'object_type':'Person', 'person_name':'mustafa'}
+
+		result = self.client_app.post("/api/addoperation", data=data, headers=headers,\
+			content_type="multipart/form-data")
+
+		
+		# close the files
+		for file in photos:
+			file[0].close()
+		
+		data = json.loads(result.data.decode())
+		
+		self.assertEqual(data['message'],  None)
+		self.assertEqual(data['status'], 'success')
+		self.assertEqual(result.content_type, 'application/json')
+		self.assertEqual(result.status_code, 201)
+
+		# the person get added
+		person = getPerson()
+
+		self.assertTrue(person)
+
+		photos = getPohto()
+
+		self.assertTrue(photos)
+		self.assertEqual(len(photos), 3)
+
+	def test_getoperation(self):
+		""" add operations then get it """
+
+		# first log-in 
+		admin_phone = os.getenv('admin_phone')
+		admin_password = os.getenv('admin_pass')
+
+		data = {'phone':admin_phone, 'password':admin_password}
+	
+		result = self.client_app.post("/api/login", data=json.dumps(data), content_type='application/json')
+
+		data = json.loads(result.data.decode())
+		
+		token = data['data']['token']
+
+		self.assertEqual(data['status'], 'success')
+		self.assertEqual(result.status_code, 200)
+
+		# add new operation
+		headers = {'token':token}
+
+		data = {'date':'2020-11-15',
+				'type_id':1, 'country_id':'1',
+				'object_type':'Person', 'person_name':'mustafa'}
+
+		result = self.client_app.post("/api/addoperation", data=data, headers=headers,\
+			content_type="multipart/form-data")
+
+		data = json.loads(result.data.decode())
+		
+		self.assertEqual(data['message'],  None)
+		self.assertEqual(data['status'], 'success')
+		self.assertEqual(result.content_type, 'application/json')
+		self.assertEqual(result.status_code, 201)
+
+		# add new operation2
+		headers = {'token':token}
+
+		data = {'date':'2020-10-25',
+				'type_id':2, 'country_id':'2',
+				'object_type':'Person', 'person_name':'test'}
+
+		result = self.client_app.post("/api/addoperation", data=data, headers=headers,\
+			content_type="multipart/form-data")
+
+		data = json.loads(result.data.decode())
+		
+		self.assertEqual(data['message'],  None)
+		self.assertEqual(data['status'], 'success')
+		self.assertEqual(result.content_type, 'application/json')
+		self.assertEqual(result.status_code, 201)
+
+
+		# get the operation
+		data = {}
+
+		result=self.client_app.get('/api/getoperation', query_string=data, content_type='application/json')
+		data = json.loads(result.data.decode())
+
+		self.assertTrue(data, 'no operations')
+		self.assertEqual(len(data['data']['operations']), 2)
+		self.assertEqual(data['message'],  None)
+		self.assertEqual(data['status'], 'success')
+		self.assertEqual(result.content_type, 'application/json')
+		self.assertEqual(result.status_code, 200)
+
+		# get the operation wuth country id filter
+		data = {'country_id':1}
+
+		result=self.client_app.get('/api/getoperation', query_string=data, content_type='application/json')
+		data = json.loads(result.data.decode())
+
+		self.assertTrue(data, 'no operations')
+		self.assertEqual(len(data['data']['operations']), 1)
+		self.assertEqual(data['message'],  None)
+		self.assertEqual(data['status'], 'success')
+		self.assertEqual(result.content_type, 'application/json')
+		self.assertEqual(result.status_code, 200)
+
+		# get the operation wuth country id filter
+		data = {'type_id':2}
+
+		result=self.client_app.get('/api/getoperation', query_string=data, content_type='application/json')
+		data = json.loads(result.data.decode())
+
+		self.assertTrue(data, 'no operations')
+		self.assertEqual(len(data['data']['operations']), 1)
+		self.assertEqual(data['message'],  None)
+		self.assertEqual(data['status'], 'success')
+		self.assertEqual(result.content_type, 'application/json')
+		self.assertEqual(result.status_code, 200)
+
+
