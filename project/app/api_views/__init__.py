a62584893bdd31f826b95eec4a323cd5b28aee3b
Mustafa Ahmed <mmustafaadn@gmail.com>
2020-07-05 19:32:51 +0200

    add 'saveFile' func

--- project/app/api_views/__init__.py
+++ project/app/api_views/__init__.py
@@ -1,9 +1,11 @@
 """This module will contain the api 'Blueprint' and its views."""
 
 from flask import Blueprint, current_app, jsonify, request, make_response
-import json
-
+import json, os
+import datetime 
 from functools import wraps
+from ..extensions import secure_filename
+
 
 # import the logic
 from ..logic.user import *
@@ -18,6 +20,9 @@ api = Blueprint('api', __name__)
 
 baseApi = {'status':None, 'message':None, 'data':{}}
 status = {'failure':'failure', 'success':'success'}
+
+MAX_IMEGES_NUMBER = 5
+ALLOWED_EXTENSIONS = ['jpg', 'png', 'jpeg']
 
 def adminRequired(f):
 	@wraps(f)
@@ -92,6 +97,29 @@ def loginRequired(f):
 
 	return mustlogin
 
+def saveFile(file):
+	''' take file and save it, return link to the photo'''
+	filename = file.filename
+	if not filename:
+		return False
+	try:
+		ext = file.filename.split('.')[-1]
+		if ext.lower() not in ALLOWED_EXTENSIONS:
+			return False
+			
+
+		filename = filename.split('.')[0] + str(datetime.datetime.now()) + '.' + ext
+		filename = secure_filename(filename)
+		path = os.path.join(current_app.config['UPLOAD_FOLDER'], filename)
+		file.save(path)
+
+		return path
+
+	except Exception as e:
+		
+		return False
+
+	
 from .user import *
 from .person import *
 from .operation import *
