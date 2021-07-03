dbb546cda548d6bda20c5dea6171d3021fa8e2ff
mustafa <mmustafaadn@gmail.com>
2021-07-03 21:35:55 +0200

    change load env way

--- project/app/__init__.py
+++ project/app/__init__.py
@@ -1,9 +1,17 @@
 from flask import Flask, make_response
 from .extensions import load_dotenv
 
+import os
+
 def create_app():
 	# load the variables from the env file!
-	load_dotenv()
+	thisPath:str = os.path.abspath(os.path.dirname(__file__))
+
+	envPath:str = os.path.abspath(os.path.join(thisPath, os.pardir))
+
+	dotenv_path = os.path.join(envPath, '.env')
+	
+	load_dotenv(dotenv_path=dotenv_path)
 
 	from . import models, api_views #, services
 	app = Flask(__name__)
